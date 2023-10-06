94ec5963c4bb0f04a58538b51b2b23d390ac6be2
Gomanosuke <VtuberGomanosuke@gmail.com>
2023-10-06 15:53:39 +0900

    unityroomで挙動変わるバグ修正

--- Assets/fruit_con.cs
+++ Assets/fruit_con.cs
@@ -83,7 +83,7 @@ public class fruit_con : MonoBehaviour
         }
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag =="fruit")
         {
